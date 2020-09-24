e6f6a1c0c83b2dded53556ec35e3b88e5af34c16
Mike Hutchinson <crikeymiles2@users.noreply.github.com>
2020-09-24 12:06:21 +0100

    v2.3 beta changes

---
+++ legacy/random_name/function.php
@@ -0,0 +1,23 @@
+<?php
+	
+function randomName() {
+	// Get the  files
+	$first = file_get_contents('random_name/first.txt');
+	$second = file_get_contents('random_name/second.txt');
+	
+	// Here we split it into lines
+	$first = explode("\n", $first);
+	shuffle($first);
+	$second = explode("\n", $second);
+	shuffle($second);
+	
+	while(strstr($second,$first)) {
+		shuffle($first);
+		shuffle($second);
+	}
+	
+	return $first[0] . ' ' . $second[0];
+	//return 'fart';
+}
+
+?>	
