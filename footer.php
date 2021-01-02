40f3a58e9995c1c6c23fcfcf822be7e8573bdb24
Mike Hutchinson <crikeymiles2@users.noreply.github.com>
2021-01-02 19:38:39 +0000

    v4

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer class="small centred">
 	<h4>Get the Gaslands: Legacy rules in <a href="https://gaslands.com/blaster" target="_blank" >BLASTER Vol.3</a></h4>
 
-	<h4><a href="https://gaslands.com/legacy/Gaslands_Legacy_DashboardCards.pdf">Dashboard Cards (PDF)</a> | <a href="https://gaslands.com/legacy/Gaslands_Legacy_Movement_Templates.pdf">Movement Templates (PDF)</a> | <a href="https://gaslands.com/legacy/secret">Unlock a Clue</a></h4>
+	<h4><a href="https://gaslands.com/legacy/Gaslands_Legacy_DashboardCards.pdf">Dashboard Cards (PDF)</a> | <a href="https://gaslands.com/legacy/Gaslands_Legacy_Movement_Templates.pdf">Movement Templates (PDF)</a> | <a href="https://gaslands.com/legacy">New Vehicle</a> | <a href="https://gaslands.com/legacy/secret">Unlock a Clue</a></h4>
 </footer>
